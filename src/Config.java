fa0bf489da3ca4a71cdbb4e76aaab5065a4cd9d0
JoseSe77-student <jpsosae@correo.url.edu.gt>
2025-08-05 14:05:17 -0600

    added conection to MySQL using Config and properties file in a .gitignore

--- src/Config.java
+++ src/Config.java
@@ -1,3 +1,20 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Config {
-    
+    private static final String CONFIG_FILE = "db_config.properties";
+    private static Properties props = new Properties();
+
+    static {
+        try {
+            props.load(new FileInputStream(CONFIG_FILE));
+        } catch (IOException e) {
+            System.out.println("No se pudo leer la configuración: " + e.getMessage());
+        }
+    }
+
+    public static String get(String key) {
+        return props.getProperty(key);
+    }
 }
